59399aac97956b0a24fa96d1be9a38d870d8c353
rhangai <hangai.renan@gmail.com>
2021-05-31 11:42:43 -0300

    chore: Added file utility on core

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,4 +4,5 @@ export * from './exception';
 export * from './types';
 export * from './create-core-module';
 export * from './create-controller-factory';
+export * from './util';
 export * from './setup';
---
+++ packages/core/src/util/file.ts
@@ -0,0 +1,22 @@
+import { Readable } from 'stream';
+
+export type FileInputType = { buffer?: Buffer } | { stream?: Readable } | { path?: string };
+export type FileInputDispatcher<T> = {
+	buffer(buffer: Buffer): T | Promise<T>;
+	stream(stream: Readable): T | Promise<T>;
+	path(filePath: string): T | Promise<T>;
+};
+
+export function fileInputDispatch<T = void>(
+	input: FileInputType,
+	dispatcher: FileInputDispatcher<T>
+) {
+	if ('buffer' in input && input.buffer) {
+		return dispatcher.buffer(input.buffer);
+	} else if ('stream' in input && input.stream) {
+		return dispatcher.stream(input.stream);
+	} else if ('path' in input && input.path) {
+		return dispatcher.path(input.path);
+	}
+	throw new Error(`Arquivo inválido`);
+}
