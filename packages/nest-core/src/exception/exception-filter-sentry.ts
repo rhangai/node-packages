843c8e561ad5756a3c1df7be3963418766ebd1ca
rhangai <hangai.renan@gmail.com>
2021-11-11 17:14:54 -0300

    fix: Sentry context

--- packages/nest-core/src/exception/exception-filter-sentry.ts
+++ packages/nest-core/src/exception/exception-filter-sentry.ts
@@ -1,3 +1,4 @@
+import { inspect } from 'util';
 import { Catch, ArgumentsHost, Optional, Type } from '@nestjs/common';
 import { InjectConfig } from '../config';
 import { ExceptionFilter } from './exception-filter';
@@ -36,10 +37,25 @@ export function createExceptionFilterSentry(
 			if (this.isSentryEnabled) {
 				const user = options.user?.(exceptionParam, host) ?? null;
 				Sentry.captureException(exceptionParam, (scope: any) => {
-					scope.setContext('host', host as any);
 					scope.setUser(user);
+					const type = host.getType<string>();
+					scope.setContext('host', { hostType: type });
+					if (type === 'http') {
+						const http = host.switchToHttp();
+						scope.setContext('http:request', prettyRequest(http.getRequest()));
+					} else if (type === 'graphql') {
+						scope.setContext('graphql', {
+							root: host.getArgByIndex(0),
+							args: host.getArgByIndex(1),
+						});
+						scope.setContext('graphql:context', host.getArgByIndex(2));
+						scope.setContext('graphql:info', host.getArgByIndex(3));
+					} else {
+						scope.setContext('host:info', {
+							args: host.getArgs(),
+						});
+					}
 					options.scope?.(scope, exceptionParam, host);
-					return scope;
 				});
 			}
 			return super.catch(exceptionParam, host);
@@ -47,4 +63,17 @@ export function createExceptionFilterSentry(
 	}
 
 	return ExceptionFilterSentryClass;
+}
+
+function prettyRequest(request: any) {
+	if (request == null) return null;
+	return {
+		url: request.url,
+		params: inspect(request.params),
+		query: inspect(request.query),
+		headers: inspect(request.rawHeaders),
+		body: inspect(request.body),
+		file: inspect(request.file),
+		files: inspect(request.files),
+	};
 }
