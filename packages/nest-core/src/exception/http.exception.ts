1e389a01a6af4d3ceb9ca06bc2ef36fcb1bbed8a
rhangai <hangai.renan@gmail.com>
2021-12-17 10:45:01 -0300

    chore: Added new PublicHttpError class

--- packages/nest-core/src/exception/http.exception.ts
+++ packages/nest-core/src/exception/http.exception.ts
@@ -1,3 +1,16 @@
 export interface ToHttpException {
 	toHttp(): Record<string, unknown> & { message: string };
 }
+
+export class PublicHttpError extends Error implements ToHttpException {
+	constructor(message?: string, private readonly httpData?: Record<string, unknown>) {
+		super(message);
+	}
+
+	toHttp(): Record<string, unknown> & { message: string } {
+		return {
+			...this.httpData,
+			message: this.message,
+		};
+	}
+}
