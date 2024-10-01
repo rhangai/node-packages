a1d371d69463c084fc8afd05bf4c202a5bae7f27
rhangai <hangai.renan@gmail.com>
2024-10-01 14:11:17 -0300

    chore: Added Utc plugin to dayjs

--- packages/core/src/core/setup.ts
+++ packages/core/src/core/setup.ts
@@ -1,6 +1,8 @@
 import dayjs from 'dayjs';
 import CustomParseFormat from 'dayjs/plugin/customParseFormat.js';
+import Utc from 'dayjs/plugin/utc.js';
 
 export function coreSetup() {
 	dayjs.extend(CustomParseFormat);
+	dayjs.extend(Utc);
 }
