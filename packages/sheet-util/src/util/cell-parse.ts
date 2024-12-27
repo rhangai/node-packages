530251cd8bfe3e6837c971dc6cab935ea306444d
rhangai <hangai.renan@gmail.com>
2024-12-27 10:21:20 -0300

    fix: Consertado formatDate quando a célula é data

--- packages/sheet-util/src/util/cell-parse.ts
+++ packages/sheet-util/src/util/cell-parse.ts
@@ -53,6 +53,6 @@ function isRawNumberFmt(fmt: NumberFormat | null | undefined) {
 function dateFormat(date: Date) {
 	const year = date.getFullYear().toString().padStart(4, '0');
 	const month = (date.getMonth() + 1).toString().padStart(2, '0');
-	const day = date.getDay().toString().padStart(2, '0');
+	const day = date.getDate().toString().padStart(2, '0');
 	return `${year}-${month}-${day}`;
 }
